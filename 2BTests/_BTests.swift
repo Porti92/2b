7b66f32833e30ceb71548e5aa4e2c4c675077c75
Nir Porti <nirporti@gmail.com>
2025-05-29 23:23:10 +0300

    Initial Commit

---
+++ 2BTests/_BTests.swift
@@ -0,0 +1,17 @@
+//
+//  _BTests.swift
+//  2BTests
+//
+//  Created by Nir Portiansky on 29/05/2025.
+//
+
+import Testing
+@testable import _B
+
+struct _BTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
